fef3bb8eed8bbadf2cc4c58426acf8df3112e414
oldtimestj <85237486@qq.com>
2021-06-01 21:21:39 +0800

    redux完整版

---
+++ src_redux完整版/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Count from './components/Count.jsx'
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <Count />
+      </div>
+    )
+  }
+}
