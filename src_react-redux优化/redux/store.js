fb5daae577fc181631c227c6357e98bdbbb9eda8
oldtimestj <85237486@qq.com>
2021-06-10 08:35:46 +0800

    react-redux优化

---
+++ src_react-redux优化/redux/store.js
@@ -0,0 +1,6 @@
+// 该文件专门用于暴露一个store对象，整个应用只有一个store对象
+import { createStore, applyMiddleware } from 'redux';
+import countReducer from './count_reducer';
+//引入redux-thunk，用于支持异步action
+import thunk from 'redux-thunk';
+export default createStore(countReducer, applyMiddleware(thunk));
